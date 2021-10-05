dd9ab7f74b16a1511f8b047a5c25ec2439410ee8
adunham95 <adunham95@gmail.com>
2021-10-05 10:35:41 -0400

    Cleaned up

--- server.js
+++ server.js
@@ -1,35 +1,9 @@
 import express  from 'express';
 import { graphqlHTTP } from 'express-graphql';
-import { buildSchema } from 'graphql';
 import schema from './schema/schema.js';
 
 // Create a server:
 const app = express();
-
-// Create a schema and a root resolver:
-// const schema = buildSchema(`
-//     type Book {
-//         title: String!
-//         author: String!
-//     }
-
-//     type Query {
-//         books: [Book]
-//     }
-// `);
-
-const rootValue = {
-    books: [
-        {
-            title: "The Name of the Wind",
-            author: "Patrick Rothfuss",
-        },
-        {
-            title: "The Wise Man's Fear",
-            author: "Patrick Rothfuss",
-        }
-    ]
-};
 
 // Use those to handle incoming requests:
 app.use('/graphql', graphqlHTTP({
